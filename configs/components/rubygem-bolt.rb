921ef0266ee424d303e4894e1b2e353275302ff7
Thomas-Franklin <thomas.franklin@puppet.com>
2019-03-21 16:18:13 +0000

    (maint) adding the dependency on to pe-bolt-server
    By adding the dependency on pe-bolt-server we can piggyback of the dependencies used as majority of them are identical.
    
    This means we will mirror the versions released with bolt-server, but also means we no longer need to package the bolt dependencies.
    
    Keeping puma as our own component so that we can still laucnh it from the ace-server bin vs the bolt-server bin.

--- configs/components/rubygem-bolt.rb
+++
@@ -1,5 +0,0 @@
-component "rubygem-bolt" do |pkg, settings, platform|
-  pkg.version "1.13.1"
-  pkg.md5sum "4fae71c9c0fbc0040234d0f897b9b067"
-  instance_eval File.read('configs/components/_base-rubygem.rb')
-end
--- configs/components/rubygem-minitar.rb
+++
@@ -1,5 +0,0 @@
-component "rubygem-minitar" do |pkg, settings, platform|
-  pkg.version "0.8"
-  pkg.md5sum "af220249c7dfe1774de3a81da4bb21d7"
-  instance_eval File.read('configs/components/_base-rubygem.rb')
-end
