78299237323bc724fa58404204d63f90ac62fefa
donoghuc <cas.donoghue@gmail.com>
2020-02-07 10:25:07 -0800

    (packaging) Add new nio4r gem for puma
    The puma 4.x series pulls in dependency on nio4r. This commit adds the gem to the ace-vanagon packaging project.

---
+++ configs/components/rubygem-nio4r.rb
@@ -0,0 +1,5 @@
+component "rubygem-nio4r" do |pkg, settings, platform|
+  pkg.version "2.5.2"
+  pkg.md5sum "fdbe3fc810a77c0182854c0e9b889962"
+  instance_eval File.read('configs/components/_base-rubygem.rb')
+end
