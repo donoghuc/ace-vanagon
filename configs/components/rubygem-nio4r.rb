b7a20a1377926cab3a428c5d4c08fbffeed90593
donoghuc <cas.donoghue@gmail.com>
2021-08-11 16:28:47 -0700

    (PE-32639) Update ace server components to latest

--- configs/components/rubygem-nio4r.rb
+++ configs/components/rubygem-nio4r.rb
@@ -1,5 +1,5 @@
 component "rubygem-nio4r" do |pkg, settings, platform|
-  pkg.version "2.5.2"
-  pkg.md5sum "fdbe3fc810a77c0182854c0e9b889962"
+  pkg.version "2.5.8"
+  pkg.md5sum "e14daa27511d8c48487e7d34c0a0d893"
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
