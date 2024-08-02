d9bf1a2daa1a7c32fc5e2eca3427e22290999c22
donoghuc <cas.donoghue@gmail.com>
2024-08-02 09:11:39 -0700

    (PE-38998) Update ace-server gem deps for 2021.7 release

--- configs/components/rubygem-nio4r.rb
+++ configs/components/rubygem-nio4r.rb
@@ -1,5 +1,5 @@
 component "rubygem-nio4r" do |pkg, settings, platform|
-  pkg.version "2.7.1"
-  pkg.md5sum "a589f31ed14a941e631484e08b85207a"
+  pkg.version "2.7.3"
+  pkg.md5sum "1143b2188d0c3271ff03805125f8339b"
   instance_eval File.read('configs/components/_base-rubygem.rb')
 end
